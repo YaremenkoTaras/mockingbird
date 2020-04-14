865e181dd057fde36ed5c7ccd33326b198659acb
Taras Yaremenko <tyaremenko@atlassian.com>
2020-04-14 21:52:59 +0300

    RRL-148: Connect shop-header to redux

--- src/components/shop-header/shop-header.js
+++ src/components/shop-header/shop-header.js
@@ -1,8 +1,9 @@
 import React from "react";
 import "./shop-header.css"
 import {Link} from "react-router-dom";
+import {connect} from "react-redux";
 
-const ShopHeader = ({itemsNum, total}) => {
+const ShopHeader = ({itemsNum, orderTotal}) => {
     return (
         <header className="shop-header">
             <Link to="/">
@@ -11,12 +12,16 @@ const ShopHeader = ({itemsNum, total}) => {
             <Link to="/cart">
                 <div className="shopping-cart">
                     <i className="cart-icon fa fa-shopping-cart"/>
-                    {itemsNum} items (${total})
+                    {itemsNum} items (${orderTotal})
                 </div>
             </Link>
         </header>
     );
 };
 
+const mapStateToProps = ({shoppingCart: {orderTotal, cartItems}}) => {
+    let itemsNum = cartItems.reduce((sum, {count}) => sum + count, 0);
+    return {orderTotal, itemsNum}
+};
 
-export default ShopHeader;
+export default connect(mapStateToProps)(ShopHeader);
