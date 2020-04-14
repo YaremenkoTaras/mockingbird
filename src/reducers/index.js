437c112452423e7448d75f772e714ee3499ad76d
Taras Yaremenko <tyaremenko@atlassian.com>
2020-04-14 22:12:04 +0300

    RRL-149: Try enhancers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,7 +2,6 @@ import updateItemList from "./item-list";
 import updateShoppingCart from "./shopping-cart";
 
 const reducer = (state, action) => {
-    console.info(`Action: ${action.type}`);
     return {
         itemList: updateItemList(state, action),
         shoppingCart: updateShoppingCart(state, action),
